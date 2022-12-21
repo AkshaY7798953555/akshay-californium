b7cf5b7ff4a22fecdada3039396972c35c3bf0dd
akshay kadam <kadama175@gmail.com>
2022-12-21 21:58:30 +0530

    fg

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,13 +9,13 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-//router.post("/createUser", UserController.createUser  )
+router.post("/createUser", UserController.createUser  )
 
-//router.get("/getUsersData", UserController.getUsersData)
+router.get("/getUsersData", UserController.getUsersData)
 
-//router.post("/createBook", BookController.createBook  )
+router.post("/createBook", BookController.createBook  )
 
-//router.get("/getBooksData", BookController.getBooksData)
+router.get("/getBooksData", BookController.getBooksData)
 router.post("/getBooksDataone", bookController1.getBooksData)
 
 module.exports=router;
