1704588df8f10444c23266e9e2653f1078867840
akshay kadam <kadama175@gmail.com>
2022-12-23 12:24:17 +0530

    hi

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -4,8 +4,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 const bookSchema = new mongoose.Schema( {
     name: String,
     author_id: {
-        type: ObjectId,
-        ref: "LibraryAuthor"
+        
     }, 
     price: Number,
     rating: Number
